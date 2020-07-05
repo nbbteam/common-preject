06ec3e3663b13e0c1e287e7ba86cc2d18785d3b1
ni82137406 <82137406>
2020-07-05 12:23:30 +0800

    tre

--- common-action/src/main/java/com/CommonActionApplication.java
+++ common-action/src/main/java/com/CommonActionApplication.java
@@ -9,8 +9,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class CommonActionApplication {
 
-
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(CommonActionApplication.class, args);
--- common-util/src/test/java/com/hikvision/boot/commonutil/CommonUtilApplicationTests.java
+++ common-util/src/test/java/com/hikvision/boot/commonutil/CommonUtilApplicationTests.java
@@ -1,7 +1,5 @@
 package com.hikvision.boot.commonutil;
 
-
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
